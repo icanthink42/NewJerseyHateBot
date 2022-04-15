9b8c2eb5be0b50516d044716f10567f3f29f281b
Ubuntu <ubuntu@ip-172-31-1-14.us-west-2.compute.internal>
2022-04-15 20:20:58 +0000

    Migrated to use the config file for all config values

---
+++ config.py
@@ -0,0 +1,6 @@
+user_save_dir = "users"
+spam_channel_id = 925208759750500392
+chance = 100
+chance2 = 4
+vc_channel_id = 925208760434192414
+banned_channels = [925208759268147326]
--- main.py
+++ main.py
@@ -9,10 +9,6 @@ import discord
 from discord.ext.tasks import loop
 
 f = open("token", "r")
-chance = 100
-chance2 = 4
-vc_channel_id = 925208760434192414
-banned_channels = [925208759268147326]
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -22,16 +18,15 @@ class MyClient(discord.Client):
             user_file = open(config.user_save_dir + "/" + user_i, "rb")
             user.users[int(user_i)] = pickle.load(user_file)
         client.join_vc.start()
-
     async def on_message(self, message):
-        if message.author.id == 964331688832417802 or  message.channel.id in banned_channels or message.author.bot:
+        if message.author.id == 964331688832417802 or  message.channel.id in config.banned_channels or message.author.bot:
             return
         if "newjersey" in message.content.lower().replace(" ",""):
             c_user = user.get_user(message.author.id)
             c_user.new_jersey_count += 1
             c_user.save()
             if "good" in message.content:
-                if random.randrange(chance2) == 0:
+                if random.randrange(config.chance2) == 0:
                     await message.reply(
                         "⚠️ **This Claim About New Jersey Is Disputed.**\nHelp keep Discord a place for reliable info. Find out more about why New Jersey is bad before posting.")
                 else:
@@ -40,13 +35,13 @@ class MyClient(discord.Client):
             else:
                 if message.channel.id == 925208759268147327: #this is the introductions channel
                     await message.reply("Oh, you’re from New Jersey? What exit?")
-                elif random.randrange(chance2) == 0:
+                elif random.randrange(config.chance2) == 0:
                     await message.reply("I̸̛̛͉͈͔̖̯̤̔̿̄͐̀͊̄̀ ̶̲͓͍͕̝͑́̒f̵̨͇͚̦̯̭̪̍͜ͅu̵̺̖̲̤̘̥͔̓c̷̢͍͒̿̒̔͑̇͠ͅk̷̨̠͓̻̠̣͕̯̩̅̌̔̆̅͌͘͝i̷̬̟̖̬̩͎̞̍ṇ̷̛͓̙̬͉̙͍̍̀̈́̀̈́̔̊͠ͅg̸̳̙̜͎̔̓̑͊͊͋̕͜͠ͅ ̸͔͚̝̳̅͛̉̓̔̆͝h̶̨̭̓̊̀̌͊̆͗̀̈́̇a̸̢̳̓͋t̶̢̛̝̫̺̭̫͍̮͍͈̍̊̈́̒̎̇̆͊̄e̸̡͉̳̲̲͓̾̅͐̈́́͗͗̓͜ͅ ̶̧̗̖͔̲̝͍͇̼͒̏̽͗̈̅̎̕n̸̗̫͙̞͗͒̆̇͂̅͆͜ḙ̸̻͕̈͆̋̉̾͌w̵̢̬̹̗̟͈̞͉͎͐̄ͅ ̸̢͉̪͍̹̼̮͚͔̐̉̌͂͂̃̚͜͠͠j̴͖͖̝͛̐̎̅̎̀̑̇͜e̴̩̦̮͙̽̓͂͐̏͝r̸͙̝̙̣͚͕̫̰̓̓̒̉͗̒ͅš̵̪̫͙͔̩̊͊̈̽̿́͋̌̽ë̸͖̹̥́̿̿̒͌̐̈́͝ỹ̷̭͍̿̈͝")
-                elif random.randrange(chance2) == 1:
+                elif random.randrange(config.chance2) == 1:
                     await message.reply("https://i.redd.it/sh4geimk2jv51.jpg")
-                elif random.randrange(chance2) == 2:
+                elif random.randrange(config.chance2) == 2:
                     await message.reply("https://www.youtube.com/watch?v=LTQpFmG2VJk")
-                elif random.randrange(chance2) == 3:
+                elif random.randrange(config.chance2) == 3:
                     await message.reply("https://www.youtube.com/watch?v=l_7XhzCc0-0")
                 else:
                     await message.reply("I fucking hate new jersey 😡😡😡😡😡😡😡😡😡")
@@ -55,18 +50,18 @@ class MyClient(discord.Client):
                 index = message.content.lower().index("im") + 3
             if "i'm " in message.content.lower():
                 index = message.content.lower().index("i'm") + 4  # stfu ignore the bad REALLY code
-            if random.randrange(chance) == 0:
+            if random.randrange(config.chance) == 0:
                 await message.reply("https://discord.com/oauth2/authorize?client_id=503720029456695306&scope=bot&permissions=537263168")
             else:
                 await message.reply(
                     "Hi " + message.content[index:] + ", I'm dad!")  # warning doesn't matter and i dont cate enough to fix
-        if random.randrange(chance) == 0:
+        if random.randrange(config.chance) == 0:
             await message.channel.send("https://images-ext-1.discordapp.net/external/2kxuirHSrAbZ9wYvmpJDF9XVoRC0cCai_5fLrhdbnf4/%3Fc%3DVjFfZGlzY29yZA/https/media.tenor.com/ZWNF4V4ftdAAAAPo/new-jersey-walter-white-amogus.mp4")
 
     @loop(seconds=3600)
     async def join_vc(self):
         audio_files = os.listdir("audio_files")
-        vc_channel = await client.fetch_channel(vc_channel_id)  # This may get me r8 limited. fix if it does
+        vc_channel = await client.fetch_channel(config.vc_channel_id)  # This may get me r8 limited. fix if it does
         vc = await vc_channel.connect()
         vc.play(discord.FFmpegPCMAudio(source="audio_files/" + audio_files[random.randrange(len(audio_files))]))
         while vc.is_playing():
